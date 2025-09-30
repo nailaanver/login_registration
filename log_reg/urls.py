399c1ab581358cec079dca0a99a1e5a6daec4061
Fathima Naila <fathimanaila71@gmail.com>
2025-09-30 20:21:36 +0530

    second commit

---
+++ log_reg/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.login,name='login'),
+    path('register',views.register_user,name ='register')
+]
--- log_reg/views.py
+++ log_reg/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
 
-def register_user(request):
-    if request.method == 'POST':
-        username = request.POST.get
 
+def login(request):
+    return render(request,'login.html')
+def register_user(request):
+    # if request.method == 'POST':
+    #     username = request.POST['username']
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+    #     password_confirm = request.POST['password_confirm']
+        
+    #     if not all([username,email,password,password_confirm]):
+    #         return render(request,'register.html',{'error': 'All fields are required'})
+    #     if password != password_confirm:
+    #         return render(request,'register.html',{'error':'Passwords do not match.'})
+    #     if User.objects.filter(username = username).exists():
+    #         return render (request,'register.html',{'error':'Username already taken.'})
+    #     if User.objects.filter(email=email).exists():
+    #         return render(request,'register.html',{'error':'Email already registered.'})
+        return render(request,'register.html')
+                          
 # Create your views here.
